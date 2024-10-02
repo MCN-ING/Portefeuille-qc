45d7591185e62320ea5dee8a0b2daf3fd8051d9d
fc-santos <filipesantos@live.ca>
2024-10-02 13:03:29 -0400

    feat: ajout de bouton 'Ajouter une attestation' dans écran Attestations
    Signed-off-by: fc-santos <filipesantos@live.ca>

--- app/src/components/AddCredentialButton.tsx
+++ app/src/components/AddCredentialButton.tsx
@@ -1,7 +1,7 @@
-import { testIdWithKey, HeaderButton, ButtonLocation } from '@hyperledger/aries-bifold-core'
+import { testIdWithKey, Button, ButtonType } from '@hyperledger/aries-bifold-core'
 import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
-import { DeviceEventEmitter } from 'react-native'
+import { View, DeviceEventEmitter, StyleSheet } from 'react-native'
 
 import { BCWalletEventTypes } from '../events/eventTypes'
 
@@ -12,14 +12,22 @@ const AddCredentialButton = () => {
     DeviceEventEmitter.emit(BCWalletEventTypes.ADD_CREDENTIAL_PRESSED, true)
   }, [])
 
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+  })
+
   return (
-    <HeaderButton
-      buttonLocation={ButtonLocation.Right}
-      accessibilityLabel={t('Credentials.AddCredential')}
-      testID={testIdWithKey('AddCredential')}
-      onPress={activateSlider}
-      icon="plus-circle-outline"
-    />
+    <View style={styles.container}>
+      <Button
+        title={t('Credentials.AddCredential')}
+        buttonType={ButtonType.Secondary}
+        testID={testIdWithKey('AddCredential')}
+        onPress={activateSlider}
+      />
+    </View>
   )
 }
 
